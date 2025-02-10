2bd226c2bd1a252d81d9122fc3d977e214691e75
Karen Chen <karenchen@microsoft.com>
2025-02-10 11:43:33 -0800

    update tracing package

--- sdk/azcore/tracing/tracing_test.go
+++ sdk/azcore/tracing/tracing_test.go
@@ -38,6 +38,10 @@ func TestProvider(t *testing.T) {
 	var setAttributesCalled bool
 	var setStatusCalled bool
 	var spanFromContextCalled bool
+	var linkFromContextCalled bool
+	var injectCalled bool
+	var extractCalled bool
+	var fieldsCalled bool
 
 	pr := NewProvider(func(name, version string) Tracer {
 		return NewTracer(func(context.Context, string, *SpanOptions) (context.Context, Span) {
@@ -46,7 +50,10 @@ func TestProvider(t *testing.T) {
 				AddLink:  func(link Link) { addLinkCalled = true },
 				SpanContext: func() SpanContext {
 					spanContextCalled = true
-					return struct{ SpanContext }{}
+					return NewSpanContext(SpanContextConfig{
+						TraceState: NewTraceState(TraceStateImpl{String: func() string { return "key1=val1,key2=val2" }}),
+						Remote:     true,
+					})
 				},
 				End:           func() { endCalled = true },
 				SetAttributes: func(...Attribute) { setAttributesCalled = true },
@@ -57,13 +64,37 @@ func TestProvider(t *testing.T) {
 				spanFromContextCalled = true
 				return Span{}
 			},
+			LinkFromContext: func(ctx context.Context, attribute ...Attribute) Link {
+				linkFromContextCalled = true
+				return Link{}
+			},
 		})
-	}, nil)
+	}, &ProviderOptions{
+		NewPropagatorFn: func() Propagator {
+			return NewPropagator(PropagatorImpl{
+				Inject: func(ctx context.Context, cr Carrier) {
+					injectCalled = true
+					cr.Set("injected", "true")
+				},
+				Extract: func(ctx context.Context, cr Carrier) context.Context {
+					extractCalled = true
+					require.EqualValues(t, "true", cr.Get("injected"))
+					return context.Background()
+				},
+				Fields: func() []string {
+					fieldsCalled = true
+					return nil
+				},
+			})
+		},
+	})
 	tr := pr.NewTracer("name", "version")
 	require.NotZero(t, tr)
 	require.True(t, tr.Enabled())
 	sp := tr.SpanFromContext(context.Background())
 	require.Zero(t, sp)
+	lk := tr.LinkFromContext(context.Background())
+	require.Zero(t, lk)
 	tr.SetAttributes(Attribute{Key: "some", Value: "attribute"})
 	require.Len(t, tr.attrs, 1)
 	require.EqualValues(t, tr.attrs[0].Key, "some")
@@ -78,6 +109,12 @@ func TestProvider(t *testing.T) {
 	sp.AddLink(Link{})
 	sc := sp.SpanContext()
 	require.NotNil(t, sc)
+	require.Zero(t, sc.TraceID())
+	require.Zero(t, sc.SpanID())
+	require.Zero(t, sc.TraceFlags())
+	require.NotNil(t, sc.TraceState())
+	require.EqualValues(t, "key1=val1,key2=val2", sc.TraceState().String())
+	require.True(t, sc.IsRemote())
 
 	sp.End()
 	sp.SetAttributes()
@@ -89,4 +126,43 @@ func TestProvider(t *testing.T) {
 	require.True(t, setAttributesCalled)
 	require.True(t, setStatusCalled)
 	require.True(t, spanFromContextCalled)
+	require.True(t, linkFromContextCalled)
+
+	tc := testCarrier{inner: map[string]string{}}
+	cr := NewCarrier(CarrierImpl{
+		Get:  tc.Get,
+		Set:  tc.Set,
+		Keys: tc.Keys,
+	})
+	pp := pr.NewPropagator()
+	pp.Inject(context.Background(), cr)
+	pp.Extract(context.Background(), cr)
+	require.Zero(t, pp.Fields())
+	require.EqualValues(t, 1, len(cr.Keys()))
+	require.True(t, injectCalled)
+	require.True(t, extractCalled)
+	require.True(t, fieldsCalled)
+}
+
+type testCarrier struct {
+	inner map[string]string
+}
+
+func (tc testCarrier) Get(key string) string {
+	if v, ok := tc.inner[key]; ok {
+		return v
+	}
+	return ""
+}
+
+func (tc testCarrier) Set(key string, value string) {
+	tc.inner[key] = value
+}
+
+func (tc testCarrier) Keys() []string {
+	keys := make([]string, 0, len(tc.inner))
+	for k := range tc.inner {
+		keys = append(keys, k)
+	}
+	return keys
 }
